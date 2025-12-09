8659bc71c978da2257be4678cee10dc58838a994
TheZombie321 <jhonnyunochoque@gmail.com>
2025-12-09 15:21:29 -0400

    filtro > 5

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\ProductoController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('productos/stock-alto', [ProductoController::class, 'stockAlto'])->name('productos.stockAlto');
 Route::resource('productos', ProductoController::class);
